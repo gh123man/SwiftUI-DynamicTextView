3029c2b087671262f06fd1d15e42d6eb2724ebdf
Brian Floersch <gh123man@gmail.com>
2022-07-05 20:48:20 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DynamicTextView
+
+A description of this package.
---
+++ Sources/DynamicTextView/DynamicTextView.swift
@@ -0,0 +1,6 @@
+public struct DynamicTextView {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
